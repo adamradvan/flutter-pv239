10f2bad939402516ee09cd77ce86b411764413f3
Adam Radvan <radvan@inqool.cz>
2021-04-08 22:00:50 +0200

    init

---
+++ burgertaste/android/app/src/main/kotlin/cz/muni/pv239/burgertaste/MainActivity.kt
@@ -0,0 +1,6 @@
+package cz.muni.pv239.burgertaste
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ homework04/android/app/src/main/kotlin/cz/muni/pv239/homework04/MainActivity.kt
@@ -0,0 +1,6 @@
+package cz.muni.pv239.homework04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
